5e7ea07bc80284f5518263975bb4e2e5f4e31447
ket.rivers <ket.rivers@proton.me>
2023-04-28 09:52:51 -0400

    update

--- 1-setup.sh
+++ 1-setup.sh
@@ -4,7 +4,7 @@
 sudo hostnamectl set-hostname openSUSE
 
 # Basic apps
-sudo zypper install -y kitty git neovim htop glances
+sudo zypper install -y kitty git neovim emacs htop glances
 
 # Fonts
 sudo zypper install -y hack-fonts fira-code-fonts
